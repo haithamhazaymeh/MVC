e2025e08c8449d826f6c06388843e2c91592077a
haithamhazaymeh <haithamhazaymeh@users.noreply.github.com>
2023-02-01 16:58:58 +0300

    task2

---
+++ MVCtask2/MVCtask2/Controllers/studentController.cs
@@ -0,0 +1,26 @@
+﻿using MVCtask2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCtask2.Controllers
+{
+    public class studentController : Controller
+    {
+        // GET: student
+        public ActionResult Index()
+        {
+
+            List<Student> ss = new List<Student>();
+
+            ss.Add(new Student() { ID = 1, Name = "mosab", Major = "civil engineer", faculity = "engineering" });
+            ss.Add(new Student() { ID = 2, Name = "ashraf", Major = "maths", faculity = "maths" });
+            ss.Add(new Student() { ID = 3, Name = "Yazeed", Major = "civil engineer", faculity = "engineering" });
+
+
+            return View(ss);
+        }
+    }
+}
---
+++ MVCtask2/MVCtask2/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCtask2.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public string Major { get; set; }
+
+        public string faculity { get; set; }
+    }
+}
